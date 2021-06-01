730bd2a37a25133c3a9f85408bcd70d81d5c9214
MichaelKvak <mkvak1987@gmail.com>
2021-06-02 01:05:30 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-   
-    <curs/>
-    <prognostic/>
-    <translate/>
-    <logpas/>
+<!--        
+   <CartDZ2
+image-src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGFf77XwVTKdTny5MsNc2mOHI5hApQ6eL9pQ&usqp=CAU"
+value="27,5"/> -->
+<CartDZ2 v-for="(item,index) in datalist" :key="index"
+:image-src="item.imageSrc"
+:value="item.value"
+/>
+  
   </div>
 </template>
 
 <script>
 
-import curs from './components/curs';
-import prognostic from './components/prognostic.vue';
-import translate from './components/translate.vue'
-import logpas from './components/logpas'
+import CartDZ2 from './components/CartDZ2/index'
+
 export default {
   name: 'App',
   components: {
-       curs,
-       prognostic,
-       translate,
-       logpas
+      CartDZ2
         
+  },
+  data() {
+    return{
+      datalist: [
+{
+  id:1,
+  imageSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGFf77XwVTKdTny5MsNc2mOHI5hApQ6eL9pQ&usqp=CAU",
+  value:"27,5"
+},
+{
+  id:2,
+  imageSrc:"https://img1.freepng.ru/20171220/joe/euro-icon-png-5a3a2409dbaae4.91687898151375975389984204.jpg",
+  value:"34" 
+}
+
+
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/CartDZ2/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <table border="1" width="20px">
+            <tr>
+                <th>Назва валюти</th>
+                <th>Курс у гривнях</th>
+            </tr>
+  <tr>
+    <td ><img :src="imageSrc" alt="currency" ></td> 
+    <td>{{value}}</td>
+  </tr>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CartDZ2',
+
+        props:{
+            imageSrc:{
+                type: String,
+                default: 'https://www.078.com.ua/upload/blog/fbe5d3fd9fca1b3accb2711209711bdc.jpg'
+            },
+
+            value:{
+                type: Number,
+                default: null
+            }
+        }
+        
+    }
+</script>
+
+<style lang="css" scoped>
+.container {
+   color: green;
+   
+   
+}
+
+</style>
