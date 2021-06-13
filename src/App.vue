0805640dc37610cdad070e5c2f3fc08ed9a25238
MichaelKvak <mkvak1987@gmail.com>
2021-06-13 19:38:22 +0300

    commit 3 task

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
     <!-- 3.Використовуємо компонент, як новий тег -->
-    <fly-menu />
+    <english-traine
+      v-for="item in cartList"
+      :key="item.id"
+      :word-image="item.wordImage"
+      :word-title="item.wordTitle"
+    />
   </div>
 </template>
 <script>
 //1. Імпортуємо файл
-import FlyMenu from "./components/DZ4/FlyMenu.vue";
+import englishTraine from "./components/DZ4/englishTraine.vue";
 export default {
   name: "App",
   components: {
     //2.Реєстрація
-    FlyMenu,
+    englishTraine,
+  },
+  data() {
+    return {
+      cartList: [
+        {
+          id: 1,
+          wordImage:
+            "https://economtochka.com.ua/image/cache/b-103_1-700x850.jpg",
+          wordTitle: "table",
+        },
+      ],
+    };
   },
 };
 </script>
---
+++ src/components/DZ4/englishTraine.vue
@@ -0,0 +1,84 @@
+<template>
+  <div :class="borderColor" class="border">
+    <span>
+      {{ result }}
+    </span>
+    <div>
+      <img :src="wordImage" alt="image" class="img" />
+    </div>
+    <div>
+      <span>{{ wordTitle }} </span>
+    </div>
+
+    <div>
+      <input type="text" placeholder="enter translation" v-model="ua" />
+      <button @click="perevod">
+        translate
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "englishTraine",
+  data() {
+    return {
+      ua: "",
+      element: "стіл",
+      result: null,
+      borderColor: null,
+    };
+  },
+  props: {
+    wordImage: {
+      type: String,
+      default: "",
+    },
+    wordTitle: {
+      type: String,
+      default: "",
+    },
+    cartList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    perevod() {
+      this.result = this.element == this.ua ? "good" : "no, try againe";
+      this.borderColor = this.ua === this.element ? "yes" : "no";
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.img {
+  width: 150px;
+  height: 150px;
+}
+.border {
+  border-width: 1;
+  border-color: rgb(8, 8, 8);
+  border-style: solid;
+  padding: 10px;
+  width: 180px;
+}
+
+.yes {
+  border-color: green;
+  border-style: solid;
+  padding: 10px;
+  width: 180px;
+  border-width: 1;
+}
+
+.no {
+  border-color: red;
+  border-width: 1;
+  border-style: solid;
+  padding: 10px;
+  width: 180px;
+}
+</style>
