e81dc865d21ba1d78ec896cbd0c7424fd29c093e
MichaelKvak <mkvak1987@gmail.com>
2021-06-14 14:45:06 +0300

    task 4 dz4

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <div>
     <!-- 3.Використовуємо компонент, як новий тег -->
-    <english-traine
-      v-for="item in cartList"
-      :key="item.id"
-      :word-image="item.wordImage"
-      :word-title="item.wordTitle"
-    />
+    <months />
   </div>
 </template>
 <script>
 //1. Імпортуємо файл
-import englishTraine from "./components/DZ4/englishTraine.vue";
+import months from "./components/DZ4/months.vue";
 export default {
   name: "App",
   components: {
     //2.Реєстрація
-    englishTraine,
+    months,
   },
   data() {
-    return {
-      cartList: [
-        {
-          id: 1,
-          wordImage:
-            "https://economtochka.com.ua/image/cache/b-103_1-700x850.jpg",
-          wordTitle: "table",
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
---
+++ src/components/DZ4/months.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    month1
+    <input type="number" v-model.number="mon1" />
+    month2
+    <input type="number" v-model.number="mon2" />
+    month3
+    <input type="number" v-model.number="mon3" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "months",
+  data() {
+    return {
+      mon1: "",
+      mon2: "",
+      mon3: "",
+    };
+  },
+  watch: {
+    mon2: function(old) {
+      this.mon3 = old + 1;
+    },
+    mon1: function(old) {
+      this.mon2 = old + 1;
+    },
+    mon3: function(old) {
+      this.mon1 = old - 2;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
